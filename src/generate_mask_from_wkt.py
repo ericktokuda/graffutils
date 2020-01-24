71fe1a70a9d9d317164e968344bf9a9b643cf270
tokudaek <tokudaek@gmail.com>
2020-01-24 14:21:43 -0300

    Update script of generation of masks to include the options to genearate the edge or just the polygon

--- src/generate_mask_from_wkt.py
+++ src/generate_mask_from_wkt.py
@@ -13,35 +13,15 @@ import numpy as np
 import cv2
 import shapely.wkt
 
-def main():
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--wktdir', required=True,
-                        help='Path to the polygons in wkt format')
-    parser.add_argument('--outdir', default='/tmp', help='Output directory')
-    args = parser.parse_args()
-
-    logging.basicConfig(format='[%(asctime)s] %(message)s',
-    datefmt='%Y%m%d %H:%M', level=logging.DEBUG)
-
-    if not os.path.exists(args.outdir): os.mkdir(args.outdir)
-
-    files = sorted(os.listdir(args.wktdir))
-    for f in files:
-        if not f.endswith('.wkt'): continue
-        wktpath = pjoin(args.wktdir, f)
-        if os.stat(wktpath).st_size == 0: continue
-        print(wktpath)
-        maskpath = pjoin(args.outdir, f.replace('.wkt', '.jpg'))
-        generate_mask_from_wkt(wktpath, maskpath)
-
 ##########################################################
-def generate_mask_from_wkt(wktpath, maskpath, img=[]):
+def draw_edge_mask_from_wkt(wktpath, maskpath, imgorig=[]):
     backgrcolor = 0
-    foregrcolor = [256, 256, 256]
+    foregrcolor = [256, 256, 256, 0.5]
 
-    if len(img) == 0:
+    if len(imgorig) == 0:
         img = np.ones((640, 640), dtype=np.uint8) * backgrcolor
 
+    img = imgorig.copy()
     polys = []
 
     wktfh = open(wktpath)
@@ -56,8 +36,98 @@ def generate_mask_from_wkt(wktpath, maskpath, img=[]):
     wktfh.close()
     polys = np.array(polys)
 
-    img = cv2.fillPoly(img, polys, foregrcolor, lineType=8, shift=0)
+    img = cv2.Canny(img,100,200)
+    # img = cv2.fillPoly(img, polys, foregrcolor, lineType=8, shift=0)
+    stencil = np.zeros(img.shape).astype(img.dtype)
+    cv2.fillPoly(stencil, polys, foregrcolor, lineType=8, shift=0)
+    img = cv2.bitwise_and(img, stencil)
     cv2.imwrite(maskpath, img)
+
+##########################################################
+def draw_mask_from_wkt(wktpath, maskpath, imgorig=[]):
+    backgrcolor = 0
+    foregrcolor = [256, 256, 256, 0.5]
+
+    if len(imgorig) == 0:
+        img = np.ones((640, 640), dtype=np.uint8) * backgrcolor
+
+    img = imgorig.copy()
+    polys = []
+
+    wktfh = open(wktpath)
+    f = shapely.wkt.loads(wktfh.read())
+
+    for poly in f:
+        coords = []
+        for x, y in zip(*poly.exterior.coords.xy):
+            coords.append([int(x), int(y)])
+        polys.append(np.array(coords))
+
+    wktfh.close()
+    polys = np.array(polys)
+
+    # img = cv2.fillPoly(img, polys, foregrcolor, lineType=8, shift=0)
+    stencil = np.zeros(img.shape).astype(img.dtype)
+    cv2.fillPoly(stencil, polys, foregrcolor, lineType=8, shift=0)
+    img = cv2.bitwise_and(img, stencil)
+    cv2.imwrite(maskpath, img)
+
+##########################################################
+def draw_polygon_from_wkt(wktpath, maskpath, imgorig=[]):
+    backgrcolor = 0
+    foregrcolor = [256, 256, 256, 0.5]
+
+    if len(imgorig) == 0:
+        img = np.ones((640, 640), dtype=np.uint8) * backgrcolor
+
+    img = imgorig.copy()
+    polys = []
+
+    wktfh = open(wktpath)
+    f = shapely.wkt.loads(wktfh.read())
+    wktfh.close()
+
+    for poly in f:
+        coords = []
+        for x, y in zip(*poly.exterior.coords.xy):
+            coords.append([int(x), int(y)])
+        polys.append(np.array(coords))
+
+    polys = np.array(polys)
+
+    for p in polys:
+        cv2.polylines(img, np.int32([p]), 1, [0, 0, 255], thickness=10)
+
+    cv2.imwrite(maskpath, img)
+
+##########################################################
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--wktdir', required=True,
+                        help='Path to the polygons in wkt format')
+    parser.add_argument('--outdir', default='/tmp', help='Output directory')
+    args = parser.parse_args()
+
+    logging.basicConfig(format='[%(asctime)s] %(message)s',
+    datefmt='%Y%m%d %H:%M', level=logging.DEBUG)
+
+    imgsdir = '/media/frodo/6TB_A/gsvcities/20180511-gsv_spcity/img'
+    if not os.path.exists(args.outdir): os.mkdir(args.outdir)
+
+    files = sorted(os.listdir(args.wktdir))
+    for f in files:
+        if not f.endswith('.wkt'): continue
+        wktpath = pjoin(args.wktdir, f)
+        if os.stat(wktpath).st_size == 0: continue
+        print(wktpath)
+        maskpath = pjoin(args.outdir, f.replace('.wkt', '_mask.jpg'))
+        edgespath = pjoin(args.outdir, f.replace('.wkt', '_edges.jpg'))
+        polypath = pjoin(args.outdir, f.replace('.wkt', '_poly.jpg'))
+        img = cv2.imread(pjoin(imgsdir, f.replace('.wkt', '.jpg')))
+
+        draw_mask_from_wkt(wktpath, maskpath, img)
+        draw_edge_mask_from_wkt(wktpath, edgespath, img)
+        draw_polygon_from_wkt(wktpath, polypath, img)
 
 if __name__ == "__main__":
     main()
