adc6ef8cb4b8631dc66a874bd2e3b44cceb8396c
tokudaek <tokudaek@gmail.com>
2020-08-27 11:21:34 -0300

    Put together kde.py and labelling.py into types.py

--- src/types.py
+++ src/types.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.collections as mc
 import matplotlib.patches as mpatches
+from mpl_toolkits.mplot3d import axes3d
 import pandas as pd
 from scipy.spatial import cKDTree
 import scipy.stats as stats
@@ -22,7 +23,8 @@ import geopandas as geopd
 import matplotlib_venn
 from src.utils import info, export_individual_axis, hex2rgb
 # plt.style.use('seaborn')
-from myutils import graph
+from myutils import graph, geo
+import scipy
 
 palettehex = ['#8dd3c7','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69']
 palette = hex2rgb(palettehex, normalized=True, alpha=1.0)
@@ -228,8 +230,11 @@ def parse_infomap_results(graphml, infomapout, labelsdf, annotator, outpath):
     """Find enclosing community given by @infomapout of each node in @graphml """
     info(inspect.stack()[0][3] + '()')
 
+    g = graph.simplify_graphml(graphml, directed=True, simplify=True)
+    vcoords = np.array([g.vs['x'], g.vs['y']]).T
+
     if os.path.exists(outpath):
-        return pd.read_csv(outpath)
+        return pd.read_csv(outpath), vcoords
 
     g = graph.simplify_graphml(graphml, directed=True, simplify=True)
     cludf = pd.read_csv(infomapout, sep=' ', skiprows=[0, 1],
@@ -244,14 +249,14 @@ def parse_infomap_results(graphml, infomapout, labelsdf, annotator, outpath):
         coords_objs[i, 1] = row.y
         i += 1
 
-    coords_nodes = np.array([g.vs['x'], g.vs['y']]).T
+    # coords_nodes = np.array([g.vs['x'], g.vs['y']]).T
 
-    kdtree = cKDTree(coords_nodes)
+    kdtree = cKDTree(vcoords)
     dists, inds = kdtree.query(coords_objs)
     labelsdf['cluster'] = np.array(cludf.cluster.tolist())[inds]
     # labelsdf['annotator'] = annotator
     labelsdf.to_csv(outpath, index=False, float_format='%.08f')
-    return labelsdf
+    return labelsdf, vcoords
 
 ##########################################################
 def convert_csv_to_annotdir(labelsclu, annotator, outdir):
@@ -352,6 +357,234 @@ def get_ratios_by_community(labelsdf, clusters, normalized=True):
     return results
 
 ##########################################################
+########################################################## KDE
+##########################################################
+
+def create_meshgrid(x, y, nx=100, ny=100, relmargin=.1):
+    """Create a meshgrid around @x and @y with @nx, @ny tiles and relative
+    margins @relmargins"""
+
+    marginx = (max(x) - min(x)) * relmargin
+    marginy = (max(y) - min(y)) * relmargin
+
+    xrange = [np.min(x) - marginx, np.max(x) + marginx]
+    yrange = [np.min(y) - marginy - .15, np.max(y) + marginy] 
+    return np.mgrid[xrange[0]:xrange[1]:(nx*1j), yrange[0]:yrange[1]:(ny*1j)]
+
+##########################################################
+def compute_pdf_over_grid(x, y, xx, yy, kerbw):
+    positions = np.vstack([xx.ravel(), yy.ravel()])
+    values = np.vstack([x, y])
+    kernel = stats.gaussian_kde(values, bw_method=kerbw)
+    return np.reshape(kernel(positions).T, xx.shape), kernel.factor
+
+#############################################################
+def plot_contours(f, x, y, xx, yy, outdir):
+    info(inspect.stack()[0][3] + '()')
+
+    xrange = [np.min(xx), np.max(xx)]
+    yrange = [np.min(yy), np.max(yy)]
+    fig, ax = plt.subplots(figsize=(8,8))
+    cfset = ax.contourf(xx, yy, f, cmap='coolwarm')
+    ax.imshow(np.rot90(f), cmap='coolwarm',
+            extent=[xrange[0], xrange[1], yrange[0], yrange[1]])
+    cset = ax.contour(xx, yy, f, colors='k')
+    ax.clabel(cset, inline=1, fontsize=10)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_xlim(xrange[0], xrange[1])
+    ax.set_ylim(yrange[0], yrange[1])
+    plt.title('2D Gaussian Kernel density estimation')
+    plt.savefig(pjoin(outdir, 'contours.pdf'))
+
+#############################################################
+def plot_surface(f, x, y, xx, yy, outdir):
+    info(inspect.stack()[0][3] + '()')
+    fig = plt.figure(figsize=(13, 7))
+    ax = plt.axes(projection='3d')
+    surf = ax.plot_surface(xx, yy, f, rstride=1, cstride=1,
+            cmap='coolwarm', edgecolor='none')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('PDF')
+    ax.set_title('Surface plot of Gaussian 2D KDE')
+    fig.colorbar(surf, shrink=0.5, aspect=5) # add color bar indicating the PDF
+    ax.view_init(60, 35)
+    plt.savefig(pjoin(outdir, 'surfaceplot.pdf'))
+
+#############################################################
+def plot_wireframe(f, x, y, xx, yy, outdir):
+    info(inspect.stack()[0][3] + '()')
+    fig = plt.figure(figsize=(13, 7))
+    ax = plt.axes(projection='3d')
+    w = ax.plot_wireframe(xx, yy, f)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('PDF')
+    ax.set_title('Wireframe plot of Gaussian 2D KDE');
+    plt.savefig(pjoin(outdir, 'wireframe.pdf'))
+
+#############################################################
+def plot_hist2d(x, y, outdir):
+    info(inspect.stack()[0][3] + '()')
+    h = plt.hist2d(x.values, y.values, density=True)
+    plt.colorbar(h[3])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Frequency histogram')
+    plt.savefig(pjoin(outdir, 'hist2d.png'))
+
+##########################################################
+def kl_divergence(p, q):
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
+
+##########################################################
+def get_points_inside_region(df, c0, radius):
+    """Get points from @df within circle of center @c0 and @radius """
+    info(inspect.stack()[0][3] + '()')
+    coords = df[['x', 'y']].values
+    
+    kdtree = cKDTree(coords)
+    inds = kdtree.query_ball_point(c0, radius)
+    return df.iloc[inds]
+    
+##########################################################
+def filename_from_coords(x, y, heading, ext='jpg'):
+    return '_{}_{}_{}.{}'.format(y, x, heading, ext)
+
+##########################################################
+def calculate_correlations(dfclulabels, accessibpath, outdir, kdeparam='scott'):
+    """Calculate correlation"""
+    info(inspect.stack()[0][3] + '()')
+
+    dfaccessib = pd.read_csv(accessibpath)
+
+    if kdeparam < 0: kdebw = 'scott'
+    else: kdebw = kdeparam
+    
+    corrs = {}
+    for l in np.unique(dfclulabels.label):
+        filtered = dfclulabels[dfclulabels.label == l]
+        
+        graffloc = np.vstack([filtered.x, filtered.y])
+        kernel = stats.gaussian_kde(graffloc, bw_method=kdebw)
+        k = ker = kernel(np.vstack([dfaccessib.x.values, dfaccessib.y.values]))
+
+        corrs[l] = []
+        for col in sorted(dfaccessib.columns):
+            if not 'accessib' in col: continue
+            acc = dfaccessib[col].values
+            corr, pvalue = scipy.stats.pearsonr(k, acc)
+            corrs[l].append(corr)
+            info('accessib{}, label{}, corr:{}'.format(col, l, corr))
+
+    accessibs = []
+    for col in sorted(dfaccessib.columns):
+        if 'accessib' in col: accessibs.append(col)
+
+    corsdf = pd.DataFrame.from_dict(corrs, orient='index', columns=accessibs)
+    corsdf.to_csv(pjoin(outdir, 'corrs.csv'))
+
+##########################################################
+def correlate_count_and_accessib(dfclulabels, accessibpath, vcoords,
+        outdir, kdebw='scott'):
+    """Plot count of graffiti vs accessibility for each vertex"""
+    info(inspect.stack()[0][3] + '()')
+
+    accs = pd.read_csv(accessibpath, header=None).values.reshape(-1)
+    
+    graffloc = np.vstack([dfclulabels.x, dfclulabels.y])
+    ker = stats.gaussian_kde(graffloc, bw_method=kdebw)
+
+    info('KERNEL dim:{}, n:{}, neff:{}, factor:{}, cov:{}'.\
+            format(ker.d, ker.n, ker.neff, ker.factor, ker.covariance))
+    
+    probs = ker(vcoords.T)
+
+    # m = np.max(accs) / 3
+    # accs[accs > 2*m] = -3
+    # accs[accs > 1*m] = -2
+    # accs[accs > 0*m] = -1
+    # accs = -accs
+
+    inds = np.argwhere(~np.isnan(accs)).flatten()
+    figscale = 6
+    fig, axs = plt.subplots(1, 1,
+                figsize=(1.2*figscale, figscale))
+    plasma = matplotlib.cm.get_cmap('plasma', 100)
+    axs.scatter(probs[inds], accs[inds], s=4, alpha=0.2, c=[plasma(.2)])
+
+    corr, _ = scipy.stats.pearsonr(probs[inds], accs[inds])
+    axs.tick_params(axis='both', which='major', labelsize='x-large')
+    axs.set_xlabel('Graffiti count', fontsize='xx-large')
+    axs.set_ylabel('Accessibility', fontsize='xx-large')
+    
+    accsuff = os.path.splitext(os.path.basename(accessibpath))[0]
+    axs.set_title('Accessib {}, KDE {:.02f}, Pearson {:.02f}'. \
+            format(accsuff, ker.factor, corr))
+    plt.tight_layout(1)
+    plt.savefig(pjoin(outdir, '{}_{:.02f}.png'.format(accsuff, ker.factor)))
+    return corr
+
+##########################################################
+def plot_densities(df, xx, yy, mapx, mapy, outdir, kerbw='scott'):
+    """Plot the densities in the grid @xx, @yy """
+    info(inspect.stack()[0][3] + '()')
+
+    labels = np.unique(df.label)
+
+    ncols = len(labels) + 1
+    figscale = 4
+    fig, axs = plt.subplots(ncols, figsize=(figscale, ncols*figscale))
+
+    pdfvals = np.ndarray((len(labels), xx.shape[0], xx.shape[1]))
+    for j, l in enumerate(labels): # compute the pdf
+        filtered = df[(df.label == int(l))]
+        pdfvals[j, :, :], _ = compute_pdf_over_grid(filtered.x,
+                filtered.y, xx, yy, kerbw)
+
+    meanpdf = np.mean(pdfvals, axis=0) # mean pdf
+    axs[0].plot(mapx, mapy, c='dimgray') # plot border
+    im = axs[ 0].scatter(xx, yy, c=meanpdf) # meand pdf plot
+    cbar = axs[0].figure.colorbar(im, ax=axs[0], fraction=0.04, pad=0.00)
+    axs[0].axis("off")
+
+    minval = 100; maxval = 0
+    for j, _ in enumerate(labels):
+        aux = pdfvals[j] - meanpdf
+        if np.min(aux) < minval: minval = np.min(aux)
+        if np.max(aux) > maxval: maxval = np.max(aux)
+
+    if minval*maxval < 0:
+        maxval = np.max(np.abs([minval, maxval]))
+        minval = -maxval
+
+    cmap = plt.cm.get_cmap('bwr').reversed()
+    kld = {}
+    for j, l in enumerate(labels):
+        jj = j + 1
+        vals = pdfvals[j] - meanpdf
+        kld[j] = kl_divergence(pdfvals[j] / np.sum(pdfvals[j]),
+                meanpdf / np.sum(meanpdf)) # normalized kld
+        axs[jj].plot(mapx, mapy, c='darkgray') # plot border
+        im = axs[jj].scatter(xx, yy, c=vals, cmap=cmap,
+                vmin=minval, vmax=maxval)
+        cbar = axs[jj].figure.colorbar(im, ax=axs[jj], fraction=0.04, pad=0.00)
+        axs[jj].axis("off")
+
+    klddf = pd.DataFrame.from_dict(kld, orient='index', columns=['kld'])
+    klddf.to_csv(pjoin(outdir, 'klds.csv'), index_label='type')
+    plt.tight_layout(2)
+    labels = ['mean', 'typeA', 'typeB', 'typeC']
+    pads = [.1, .1, .6, .1]
+
+    if type(kerbw) == str: pref = 'ker_{}_'.format(kerbw)
+    else: pref = 'ker_{:.02f}_'.format(kerbw)
+
+    export_individual_axis(axs, fig, labels, outdir, pad=pads,
+            prefix=pref, fmt='png')
+
+##########################################################
 def main():
     info(inspect.stack()[0][3] + '()')
     t0 = time.time()
@@ -368,10 +601,14 @@ def main():
     cluareaspath = './data/20200202-types/20200222-infomap_areas.csv'
     outlabels = pjoin(args.outdir, 'labels.csv')
     outlabelsclu = 'data/labels_and_clu_nodupls.csv'
+    c0 = [-46.6333, -23.5505] # Sao paulo
 
-    # labelsdf = compile_labels(annotdir, outlabels) # Do this for each annotator
-    # labelsdf = parse_infomap_results(graphmlpath, clupath, labelsdf,
-            # 'er', outlabelsclu)
+    shppath = './data/shp/'
+    accpath = './data/accessib/acc15.txt'
+
+    labelsdf = compile_labels(annotdir, outlabels) # Do this for each annotator
+    labelsdf, vcoords = parse_infomap_results(graphmlpath, clupath, labelsdf,
+            'er', outlabelsclu)
 
     # plot_types(clupath, shppath, outlabelsclu, args.outdir)
     # clus = sorted(np.unique(labelsdf.cluster))
@@ -382,7 +619,23 @@ def main():
     # plot_counts_normalized(outlabelsclu, cluareaspath, args.outdir)
     # plot_venn(outlabelsclu, args.outdir)
 
+    # Kernel density estimation
     info('Elapsed time:{}'.format(time.time()-t0))
+    xx, yy = create_meshgrid(labelsdf.x, labelsdf.y, relmargin=.1)
+    kerbw = .3
+    pdf, _ = compute_pdf_over_grid(labelsdf.x, labelsdf.y, xx, yy, kerbw)
+    # plot_contours(pdf, labelsdf.x, labelsdf.y, xx, yy, args.outdir)
+    # plot_surface(pdf, labelsdf.x, labelsdf.y, xx, yy, args.outdir)
+    # plot_wireframe(pdf, labelsdf.x, labelsdf.y, xx, yy, args.outdir)
+    # plot_hist2d(labelsdf.x, labelsdf.y, args.outdir)
+    mapx, mapy = geo.get_shp_points(shppath)
+    # plot_densities(labelsdf, xx, yy, mapx, mapy, args.outdir, kerbw)
+    for kerbw in np.arange(.05, .41, .05):
+        info('kerbw:{}'.format(kerbw))
+        corr = correlate_count_and_accessib(labelsdf, accpath, vcoords, args.outdir, kerbw)
+        info('corr:{}'.format(corr))
+
+    # calculate_correlation(df, accessibpath, args.outdir, .3)
 
 ##########################################################
 if __name__ == "__main__":
